e7f681e41067113a36343e589efafa3e22d86f0b
OfekAgmon <ofek.agmon@gmail.com>
2015-10-13 23:34:35 +0300

    Commit for first deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn RidesBackend.wsgi:application
--- RidesBackend/settings.py
+++ RidesBackend/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -66,8 +67,6 @@ ROOT_URLCONF = 'RidesBackend.urls'
 
 WSGI_APPLICATION = 'RidesBackend.wsgi.application'
 
-
-
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
@@ -81,6 +80,8 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+DATABASES['default'] = dj_database_url.config()
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
